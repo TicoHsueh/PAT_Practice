e765e4fb8464368ddb60b62d867b872cba2da63f
TicoHsueh <HsuehBo@outlook.com>
2019-08-28 21:49:35 +0800

    0828 Submit

--- PAT/A1034 Head of a Gang.cpp
+++ PAT/A1034 Head of a Gang.cpp
@@ -2,8 +2,9 @@
 #define MAXN 3000
 using namespace std;
 int N,K;
-map<string,int> node;
+map<string,int> node,numOfMem;
 map<int,string> intToStr;
+map<string,vector<string> > MemList;
 set<string> nameSet;
 int father[MAXN];
 
@@ -26,7 +27,7 @@ int findFather(int x)
 
 void init()
 {
-    for(int i=0;i<MAXN;i++)
+    for(int i=0; i<MAXN; i++)
         father[i] = i;
 }
 
@@ -34,7 +35,8 @@ void Union(int a,int b)
 {
     int fa = findFather(a);
     int fb = findFather(b);
-    if(fa!=fb){
+    if(fa!=fb)
+    {
         father[fa] = fb;
     }
     return ;
@@ -43,7 +45,8 @@ void Union(int a,int b)
 int hashName(string s)
 {
     int ans = 0;
-    for(int i=0;i<s.length();i++){
+    for(int i=0; i<s.length(); i++)
+    {
         ans += (s[i]-'A')*26 + i;
     }
     return ans;
@@ -53,7 +56,8 @@ int main()
 {
     cin>>N>>K;
     init();
-    for(int i=0;i<N;i++){
+    for(int i=0; i<N; i++)
+    {
         string nameA,nameB;
         int Aid,Bid;
         int length;
@@ -63,31 +67,47 @@ int main()
         intToStr[Aid] = nameA;
         intToStr[Bid] = nameB;
         Union(Aid,Bid);
-        if(node.count(nameA)==0){
+        if(node.count(nameA)==0)
+        {
             node[nameA] = 0;
         }
-        if(node.count(nameB)==0){
+        if(node.count(nameB)==0)
+        {
             node[nameB] = 0;
         }
         node[nameA] += length;
         node[nameB] += length;
     }
     map<string,int>::iterator it;
-    for(it=node.begin();it!=node.end();it++){
+    for(it=node.begin(); it!=node.end(); it++)
+    {
         string s = it->first;
         int id = hashName(s);
         int t = findFather(id);
-        printf("%s's Fa:%d(%s)\n",s.c_str(),t,intToStr[t].c_str());
+        string faName = intToStr[t];
+        if(numOfMem.count(faName)==0)
+        {
+            numOfMem[faName] = 0;
+        }
+        numOfMem[faName]++;
+        MemList[faName].push_back(s);
+//        printf("%s's Fa:%d(%s)\n",s.c_str(),t,intToStr[t].c_str());
     }
-//    for(int i=0;i<MAXN;i++){
-////        printf("%d:Fa:%d\n",i,findFather(i));
-//        if(findFather(i)==i&&intToStr.count(i)!=0){
-//            printf("%d %s\n",i,intToStr[i].c_str());
-//        }
-////        if(intToStr.count(i)!=0){
-////            printf("%d %s\n",i,intToStr[i].c_str());
-////        }
-//    }
+    int maxLen = 0;
+    for(it=numOfMem.begin(); it!=numOfMem.end(); it++)
+    {
+        if(it->second>2)
+        {
+            cout<<it->first<<" "<<it->second<<endl;
+            vector<string> temp = MemList[it->first];
+            for(int i=0; i<temp.size(); i++)
+            {
+                cout<<temp[i]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
 
     return 0;
 }
